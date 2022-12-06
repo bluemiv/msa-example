39ca4d05bd3aa2938192d4421b545b9ef4a6207e
taehongkim <public.bluemiv@gmail.com>
2022-12-06 21:56:53 +0900

    feat: user backend service와 연동

--- backend/user/src/main/java/com/bluemiv/user/interfaces/UserController.java
+++ backend/user/src/main/java/com/bluemiv/user/interfaces/UserController.java
@@ -2,18 +2,27 @@ package com.bluemiv.user.interfaces;
 
 import com.bluemiv.user.domain.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/")
 public class UserController {
 
-    @GetMapping("user")
-    public @ResponseBody User user() {
-        return User.builder().id(1).username("taehongkim").email("public.bluemiv@gmail.com").build();
+    @GetMapping("user/{id}")
+    public @ResponseBody User user(@PathVariable long id) {
+        log.debug("API: user/"+ id);
+        return User.builder().id(id).username("taehongkim").email("public.bluemiv@gmail.com").build();
+    }
+
+    @GetMapping("users")
+    public @ResponseBody ArrayList<User> users() {
+        log.debug("API: users");
+        final ArrayList<User> users = new ArrayList<>();
+        users.add(User.builder().id(1).username("taehongkim").email("taehong0115@gmail.com").build());
+        users.add(User.builder().id(2).username("bluemiv").email("public.bluemiv@gmail.com").build());
+        return users;
     }
 }
