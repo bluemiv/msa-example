d8449a1a7d7bcc4d94c76109d07d2bd908812047
taehongkim <public.bluemiv@gmail.com>
2022-12-06 20:38:13 +0900

    feat: Add user server

--- backend/user/src/main/java/com/bluemiv/user/domain/User.java
+++ backend/user/src/main/java/com/bluemiv/user/domain/User.java
@@ -1,2 +1,18 @@
-package com.bluemiv.user.domain;public class User {
+package com.bluemiv.user.domain;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class User {
+
+    private long id;
+
+    private String username;
+    private String password;
+
+    private String email;
 }
--- backend/user/src/main/java/com/bluemiv/user/interfaces/UserController.java
+++ backend/user/src/main/java/com/bluemiv/user/interfaces/UserController.java
@@ -1,2 +1,19 @@
-package com.bluemiv.user.interfaces;public class UserController {
+package com.bluemiv.user.interfaces;
+
+import com.bluemiv.user.domain.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/")
+public class UserController {
+
+    @GetMapping("user")
+    public @ResponseBody User user() {
+        return User.builder().id(1).username("taehongkim").email("public.bluemiv@gmail.com").build();
+    }
 }
